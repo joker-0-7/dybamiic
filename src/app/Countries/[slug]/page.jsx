33e0c181cdcb34a5d105a6ffdc8ca7d928af9b04
joker-0-7 <jokere136@gmail.com>
2023-08-27 18:27:24 +0300

    fdf

--- src/app/Countries/[slug]/page.jsx
+++ src/app/Countries/[slug]/page.jsx
@@ -7,20 +7,34 @@ import { statesJson } from "../page"
 
 //     return states
 // }
+export async function generateMetadata({ params, searchParams }, parent) {
+  const id = params.slug
+ 
+  const product = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json())
+ 
+  const previousImages = (await parent).openGraph?.images || []
+ console.log(product);
+  return {
+    title: product.title,
+    openGraph: {
+      images: ['/some-specific-page-image.jpg', ...previousImages],
+    },
+  }
+}
 
 export default async function page({params}) {
 
     // const states = fetchStates(params)
   return (
-    // <div>
-    //   <h1>About {states.name.official} flag</h1>
-    //   <Image src={states.flags.svg} width={300} height={300} alt={states.flags.alt}/>
+    <div>
+      {/* <h1>About {states.name.official} flag</h1>
+      <Image src={states.flags.svg} width={300} height={300} alt={states.flags.alt}/>
 
-    //   <p>{states.flags.alt}</p>
+      <p>{states.flags.alt}</p>
 
-    //   <p>Region: {states.region}</p>
-    //   <p>Capital: {states.capital}</p>
-    // </div>
-    <h1>{params}</h1>
+      <p>Region: {states.region}</p>
+      <p>Capital: {states.capital}</p> */}
+      <h1>{params.slug}</h1>
+    </div>
   )
 }
--- src/app/Countries/page.jsx
+++ src/app/Countries/page.jsx
@@ -7,16 +7,14 @@ async function nations(){
     const response = await fetch('https://restcountries.com/v3.1/all')
 
     const data = await response.json()
-
     statesJson = data.slice()
 
     return data
 }
 
 export default async function Countries() {
-  
+    
     const data = await nations()
-
     const africa = data.filter(country => country.continents.includes('Africa'))
 
     const countries = data.map(country => (
