80706e9347fc3fd443a427f670861fa71aaef439
joker-0-7 <jokere136@gmail.com>
2023-08-27 17:35:53 +0300

    fg

--- src/app/Countries/[slug]/page.jsx
+++ src/app/Countries/[slug]/page.jsx
@@ -12,14 +12,15 @@ export default async function page({params}) {
 
     const states = fetchStates(params)
   return (
-    <div>
-      <h1>About {states.name.official} flag</h1>
-      <Image src={states.flags.svg} width={300} height={300} alt={states.flags.alt}/>
+    // <div>
+    //   <h1>About {states.name.official} flag</h1>
+    //   <Image src={states.flags.svg} width={300} height={300} alt={states.flags.alt}/>
 
-      <p>{states.flags.alt}</p>
+    //   <p>{states.flags.alt}</p>
 
-      <p>Region: {states.region}</p>
-      <p>Capital: {states.capital}</p>
-    </div>
+    //   <p>Region: {states.region}</p>
+    //   <p>Capital: {states.capital}</p>
+    // </div>
+    <h1>{params}</h1>
   )
 }
