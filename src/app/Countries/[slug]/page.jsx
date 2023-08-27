5bfad38de1007a33d00e5fe660125f384cdbca63
joker-0-7 <jokere136@gmail.com>
2023-08-27 17:39:22 +0300

    bb

--- src/app/Countries/[slug]/page.jsx
+++ src/app/Countries/[slug]/page.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image"
 import { statesJson } from "../page"
 
-function fetchStates(params){
+// function fetchStates(params){
  
-    const states = statesJson.find((state)=>state.cca3 === params.slug)
+//     const states = statesJson.find((state)=>state.cca3 === params.slug)
 
-    return states
-}
+//     return states
+// }
 
 export default async function page({params}) {
 
-    const states = fetchStates(params)
+    // const states = fetchStates(params)
   return (
     // <div>
     //   <h1>About {states.name.official} flag</h1>
